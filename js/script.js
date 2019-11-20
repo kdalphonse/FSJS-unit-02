359d70c5d300f2acaea71084996406dcb1a7d510
Kevin Dalphonse <kevin.dalphonse@e-closing.com>
2019-11-20 12:41:45 -0500

    Manipulate 'active' pagination button.  Update comments

--- js/script.js
+++ js/script.js
@@ -6,19 +6,7 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 const items_per_page = 10;  // number of students to display per page
-
 let students = document.querySelectorAll('.student-item'); // list of students on the page
 
 
@@ -49,52 +37,50 @@ function showPage(student_list, page) {
 
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+   Add pagination 'buttons' and functionality.
 ***/
 
 const appendPageLinks = (list) => {
-   /*
-   1. Determine how many pages are needed for the list by dividing the
-   total number of list items by the max number of items per page
-   2. Create a div, give it the “pagination” class, and append it to the .page div
-   3. Add a ul to the “pagination” div to store the pagination links
-   4. for every page, add li and a tags with the page number text
-   5. Add an event listener to each a tag. When they are clicked
-   call the showPage function to display the appropriate page
-   6. Loop over pagination links to remove active class from all links
-   7. Add the active class to the link that was just clicked. You can identify that
-   clicked link using event.target
-   */
-
    const num_pages = Math.ceil(list.length / items_per_page);
 
+   // create a div for the pagination buttons
    const pagination_div = document.createElement('div');
    pagination_div.className = 'pagination';
 
+   // add an ordered list to the pagination div
+   const button_list = document.createElement('ul');
+   pagination_div.appendChild(button_list);
+
+   // add the paginatin div to the page
    const page_div = document.querySelector('.page');
    page_div.appendChild(pagination_div);
 
-   const links_list = document.createElement('ul');
-   pagination_div.appendChild(links_list);
+   // create and add each individual pagination button to the list
+   for (let i = 1; i <= num_pages; i++) {      
+      const button = document.createElement('a');
 
-   for (let i = 1; i <= num_pages; i++) {
+      button.textContent = i;
+      if (i === 1) {
+         button.className = 'active'; // make the first one active by default
+      }
+
+      // when clicked, change the 'active' button and show the related page
+      button.addEventListener("click", (event) => { 
+         // find and de-activate currently active button
+         let active_button = button_list.querySelector('a.active');
+         active_button.className = '';
+
+         // find and activate new active button
+         event.target.className = 'active';
+
+         showPage(list, i);
+      })
+
       const li = document.createElement('li');
-      const link = document.createElement('a');
-
-      li.appendChild(link);
-      link.textContent = i;
-      link.addEventListener("click", () => { showPage(list, i) })
-
-      links_list.appendChild(li);
+      li.appendChild(button);
+      button_list.appendChild(li);
    }
-
-
-
-
 }
 
 appendPageLinks(students);
-showPage(students, 1);
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+showPage(students, 1); // show the first page by default
