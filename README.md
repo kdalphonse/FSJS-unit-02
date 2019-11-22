1d8c95752056ba240e75c185dbcbc8429b704399
Kevin Dalphonse <kevin.dalphonse@e-closing.com>
2019-11-22 12:55:58 -0500

    Added basic search
    Search does not work with pagination yet

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Treehouse Full Stack JavaScript Techdegree project 2.
 The objective of this project was to add pagination to a web page that has a list of 'students', displaying 10 students to a page.
 
 By using Unobtrusive JavaScript (and Progressive Enhancement in particular), the page should 'work' with or without javascript enabled.
+
--- js/script.js
+++ js/script.js
@@ -11,17 +11,44 @@ let students = document.querySelectorAll('.student-item'); // list of students o
    Only display students for the current 'page', hide the rest
 ***/
 
-function showPage(student_list, page) {
+function showPage(student_list, page, filtered) {
    const start = (page - 1) * items_per_page // index of first student on the page
    let end = page * items_per_page - 1       // index of last student on the page
    
-   for( i = 0; i < student_list.length; i++) {
+   for( let i = 0; i < student_list.length; i++) {
+      if (!filtered || student_list[i].style.display === '') {
       const show = i >= start && i <= end;
       student_list[i].style.display = show ? '' : 'none';
+      }
    }   
 }
 
+function showFilteredPage(student_list, page) {
+   let num_found = 0;
+   
+   for( let i = 0; i < student_list.length; i++) {
+      if (student_list[i].style.display === '') {
+         const show = num_found < items_per_page;
+         student_list[i].style.display = show ? '' : 'none';
+         num_found++;
+      }
+   }   
+}
 
+/***
+   Only select students whose names match the filter
+ ***/
+function filterList(student_list, filter) {
+   let new_student_list = [];
+
+   for( let i = 0; i < student_list.length; i++) {
+      // students name is in the h3 element of the div inside the list item.
+      const student_name = student_list[i].querySelector('div h3').textContent;
+
+      const show = student_name.indexOf(filter) !== -1;
+      student_list[i].style.display = show ? '' : 'none';
+   }   
+}
 /*** 
    Add pagination 'buttons' and functionality.
 ***/
@@ -68,5 +95,43 @@ const appendPageLinks = (list) => {
    }
 }
 
+/***
+   Add a search input field and button
+***/
+
+function appendSearch() {
+
+   // search input/prompt
+   const input = document.createElement('input');
+   input.placeholder = 'Search for students...';
+
+   // search button
+   const button = document.createElement('button');
+   button.textContent = 'Search';
+   button.addEventListener('click', (event) => { 
+      const filterValue = input.value;
+      if (filterValue === '') {
+         showPage(students,1);
+      }
+      else {
+         filterList(students, filterValue);
+         showFilteredPage(students, 1);
+      }
+   });
+
+   // create a div to hold the input and button
+   const searchDiv = document.createElement('div');
+   searchDiv.className = 'student-search';
+
+   // append prompt and button to the div
+   searchDiv.appendChild(input);
+   searchDiv.appendChild(button);
+
+   // append the div to the header
+   const header = document.querySelector('.page-header');
+   header.appendChild(searchDiv);
+}
+
+appendSearch();
 appendPageLinks(students);
 showPage(students, 1); // show the first page by default
